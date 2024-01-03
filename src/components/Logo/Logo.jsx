816de03ba6aabab83cb82323864fc8f2e7761cf5
VishaShinde <vishashinde04@gmail.com>
2024-01-03 16:46:17 +0530

    First commit

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import QtifyLogo from "../../assets/QtifyLogo.png";
+
+export default function Logo(){
+    return <img src={QtifyLogo} alt="logo" width={67}/>
+}
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Button from "../Button/button";
 import Search from "../Search/search";
-import {ReactComponent as QtifyLogo} from "../../assets/QtifyLogo.svg"
+import QtifyLogo from "../Logo/Logo";
 import styles from "./navbar.module.css"
 
 function Navbar({searchData}){
   return(
-    <nav >
-        <a href="/"><QtifyLogo className={styles.logo}/></a>
-        <Search className={styles.searchbar} placeholder={"Search a album of your choice"} searchData={searchData}/>
+    <nav className={styles.navbar} >
+        <a href="/">
+          <QtifyLogo />
+        </a>
+        <Search  
+          placeholder={"Search a album of your choice"} 
+          searchData={searchData}/>
         <Button>Give Feedback</Button>
     </nav>
   )
